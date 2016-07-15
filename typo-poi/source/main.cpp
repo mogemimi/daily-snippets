f5a19ea292e25d0c581416a0e75efdca9616504b
mogemimi <mogemimi@enginetrouble.net>
2016-07-16 04:22:29 +0900

    Refine the tool

--- typo-poi/source/main.cpp
+++ typo-poi/source/main.cpp
@@ -51,8 +51,8 @@ void ReadUTF8TextFile(
     std::istreambuf_iterator<char> end;
     std::size_t byteIndex = 0;
     std::size_t utf8CharacterCount = 0;
-    std::size_t lineCount = 0;
-    std::size_t columnCount = 0;
+    std::size_t lineCount = 1;
+    std::size_t columnCount = 1;
 
     while (input && (byteIndex < fileSize)) {
         std::array<UTF8, 5> buffer;
@@ -68,7 +68,15 @@ void ReadUTF8TextFile(
 
         if (!isLegalUTF8Sequence(source, sourceEnd)) {
             // Error: Invalid UTF-8 byte sequence
-            std::cerr << "error: Invalid UTF-8 byte sequence." << std::endl;
+            std::cerr
+                << "error: Invalid UTF-8 byte sequence at "
+                << path
+                << ":"
+                << lineCount
+                << ":"
+                << columnCount
+                << "."
+                << std::endl;
             break;
         }
 
@@ -96,7 +104,7 @@ void ReadUTF8TextFile(
 
         if (buffer.front() == '\n') {
             lineCount++;
-            columnCount = 0;
+            columnCount = 1;
         }
     }
 }
@@ -135,8 +143,54 @@ bool IsSeparator(const std::string& c)
     return std::binary_search(std::begin(separators), std::end(separators), c.front());
 }
 
-void ReadTextFileWithoutPedanticMode(const std::string& path)
+void ReadTextFileWithoutPedanticMode(somera::TypoMan & typos, const std::string& path)
 {
+    auto onWord = [&](const std::string& sourceString) {
+        somera::TypoSource source;
+        source.file = path;
+        typos.computeFromSentence(sourceString, source);
+    };
+    auto flush = [&](std::string && text) {
+        somera::WordSegmenter segmenter;
+        segmenter.parse(text, [&](const somera::PartOfSpeech& word) {
+            onWord(word.text);
+        });
+    };
+    std::string word;
+    ReadUTF8TextFile(path, [&](Character && character) {
+        if (IsSpace(character.word) || IsSeparator(character.word)) {
+            if (!word.empty()) {
+                std::string temp;
+                std::swap(word, temp);
+                flush(std::move(temp));
+            }
+            return;
+        }
+        word += character.word;
+    });
+}
+
+} // unnamed namespace
+
+int main(int argc, char *argv[])
+{
+    CommandLineParser parser;
+    SetupCommandLineParser(parser);
+    parser.parse(argc, argv);
+
+    if (parser.hasParseError()) {
+        std::cerr << parser.getErrorMessage() << std::endl;
+        return 1;
+    }
+    if (parser.exists("-h") || parser.exists("-help")) {
+        std::cout << parser.getHelpText() << std::endl;
+        return 0;
+    }
+    if (parser.getPaths().empty()) {
+        std::cerr << "error: no input file" << std::endl;
+        return 1;
+    }
+
     somera::TypoMan typos;
     typos.setStrictWhiteSpace(false);
     typos.setStrictHyphen(false);
@@ -217,54 +271,21 @@ void ReadTextFileWithoutPedanticMode(const std::string& path)
         }
         std::printf("\n");
     });
-    auto onWord = [&](const std::string& sourceString) {
-        somera::TypoSource source;
-        source.file = path;
-        typos.computeFromSentence(sourceString, source);
-    };
-    auto flush = [&](std::string && text) {
-        somera::WordSegmenter segmenter;
-        segmenter.parse(text, [&](const somera::PartOfSpeech& word) {
-            onWord(word.text);
-        });
-    };
-    std::string word;
-    ReadUTF8TextFile(path, [&](Character && character) {
-        if (IsSpace(character.word) || IsSeparator(character.word)) {
-            if (!word.empty()) {
-                std::string temp;
-                std::swap(word, temp);
-                flush(std::move(temp));
-            }
-            return;
-        }
-        word += character.word;
-    });
-}
-
-} // unnamed namespace
-
-int main(int argc, char *argv[])
-{
-    CommandLineParser parser;
-    SetupCommandLineParser(parser);
-    parser.parse(argc, argv);
-
-    if (parser.hasParseError()) {
-        std::cerr << parser.getErrorMessage() << std::endl;
-        return 1;
-    }
-    if (parser.exists("-h") || parser.exists("-help")) {
-        std::cout << parser.getHelpText() << std::endl;
-        return 0;
-    }
-    if (parser.getPaths().empty()) {
-        std::cerr << "error: no input file" << std::endl;
-        return 1;
-    }
 
     for (auto & path : parser.getPaths()) {
-        ReadTextFileWithoutPedanticMode(path);
+        auto ext = std::get<1>(somera::FileSystem::splitExtension(path));
+        if (ext == ".png"
+            || ext == ".jpg"
+            || ext == "exe"
+            || ext == ".DS_Store"
+            || ext == ".o"
+            || ext == ".a"
+            || ext == ".d"
+            || ext == ".lib"
+            || ext == ".dll") {
+            continue;
+        }
+        ReadTextFileWithoutPedanticMode(typos, path);
     }
 
     return 0;
